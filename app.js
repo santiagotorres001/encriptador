7d2316ca520f642e5a6da43d8354e4f4c4b4a401
Santiago <felipetorres260@gmail.com>
2024-08-18 13:21:31 -0500

    actualizando JS

---
+++ app.js
@@ -0,0 +1,64 @@
+function encriptar(texto) {
+    if (/[^a-z\s]/.test(texto)) {
+        return "El texto contiene caracteres no permitidos.";
+    }
+
+    const encriptado = texto
+        .replace(/e/g, "enter")
+        .replace(/i/g, "imes")
+        .replace(/a/g, "ai")
+        .replace(/o/g, "ober")
+        .replace(/u/g, "ufat");
+
+    return encriptado;
+}
+
+function desencriptar(texto) {
+    // Reemplazamos las secuencias encriptadas en el orden inverso
+    const desencriptado = texto
+        .replace(/ufat/g, "u")
+        .replace(/ober/g, "o")
+        .replace(/ai/g, "a")
+        .replace(/imes/g, "i")
+        .replace(/enter/g, "e");
+
+    return desencriptado;
+}
+
+function encriptarTexto() {
+    const inputTexto = document.getElementById("inputTexto").value;
+    const resultado = encriptar(inputTexto);
+    if(inputTexto == ""){
+        alert("No hay texto en el area.");
+    }
+    else{
+        document.getElementById("outputTexto").value = resultado;
+        
+        document.getElementById("copiar").style.display = "inline-block";
+        document.getElementById("ocultar").style.display = "none";
+    }
+   
+}
+
+function desencriptarTexto() {
+    const inputTexto = document.getElementById("inputTexto").value;
+    const resultado = desencriptar(inputTexto);
+    if (inputTexto == ""){
+        alert("No hay texto en el area.");
+    }
+    else{
+        document.getElementById("outputTexto").value = resultado;
+        document.getElementById("copiar").style.display = "inline-block";
+        document.getElementById("ocultar").style.display = "none";
+    }
+    
+}
+
+function copiarTexto() {
+    const outputTexto = document.getElementById("outputTexto");
+    outputTexto.select();
+    outputTexto.setSelectionRange(0, 99999); // Para dispositivos móviles
+    document.execCommand("copy");
+
+    alert("Texto copiado al portapapeles: " + outputTexto.value);
+}
